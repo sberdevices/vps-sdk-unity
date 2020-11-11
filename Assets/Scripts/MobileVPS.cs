dc3290ecb32374b2368d57ffb918a4c9c2fd22d1
Nikita Lukyantsev <>
2020-11-11 13:34:37 +0300

    Добавил зависимости для запуска нейронок

---
+++ Assets/Scripts/MobileVPS.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace TensorFlowLite
+{
+    public class MobileVPS : MonoBehaviour
+    {
+        private const string FileName = "hfnet_i8_960.tflite";
+        public Texture2D TestTexture;
+
+        Interpreter interpreter;
+
+        private void Start()
+        {
+            var options = new InterpreterOptions
+            {
+                threads = 2,
+            };
+
+            interpreter = new Interpreter(FileUtil.LoadFile(FileName), options);
+            interpreter.AllocateTensors();
+        }
+
+        private void OnDestroy()
+        {
+            interpreter?.Dispose();
+        }
+
+        public async Task<float[,]> GetFeaturesAsync(float[,,] input)
+        {
+            return await Task.Run(() => doInference(input));
+        }
+
+        public float[,] doInference(float [,,] input)
+        {
+            Debug.Log("Start");
+            var idim0 = interpreter.GetInputTensorInfo(0).shape;
+            var height = idim0[1]; //960
+            var width = idim0[2]; //540
+            var channels = idim0[3]; //1
+
+            //var input0 = new float[height, width, channels];
+
+            //var pixels = TestTexture.GetPixels();
+            //for (int i = 0; i < pixels.Length; i++)
+            //{
+            //    input0[i / width, i % width, 0] = (float)(pixels[i].r * 255);
+            //}
+
+            interpreter.SetInputTensorData(0, input);
+
+            var output0 = new float[4096];
+            var output1 = new float[400, 2];
+            var output2 = new float[400, 256];
+            var output3 = new float[400];
+
+            interpreter.Invoke();
+
+            interpreter.GetOutputTensorData(0, output0);
+            interpreter.GetOutputTensorData(1, output1);
+            interpreter.GetOutputTensorData(2, output2);
+            interpreter.GetOutputTensorData(3, output3);
+
+            Debug.Log("Done");
+            return output1;
+
+            //Texture2D tex = FeatureTexture.mainTexture as Texture2D;
+
+            //for (int i = 0; i < output1.Length / 2; i++)
+            //{
+            //    tex.SetPixel((int)output1[i, 0], (int)output1[i, 1], Color.yellow);
+            //    Debug.Log(output1[i, 0] + ":" + output1[i, 1]);
+            //}
+
+            //for (int i = 0; i < tex.height; i++)
+            //    for (int j = 0; j < tex.width; j++)
+            //    {
+            //        if (tex.GetPixel(j, i) == Color.yellow)
+            //        {
+            //            tex.SetPixel(j - 1, i - 1, Color.green);
+            //            tex.SetPixel(j - 1, i, Color.green);
+            //            tex.SetPixel(j - 1, i + 1, Color.green);
+            //            tex.SetPixel(j, i - 1, Color.green);
+            //            tex.SetPixel(j, i + 1, Color.green);
+            //            tex.SetPixel(j + 1, i - 1, Color.green);
+            //            tex.SetPixel(j + 1, i, Color.green);
+            //            tex.SetPixel(j + 1, i + 1, Color.green);
+            //        }
+            //    }
+            //tex.Apply();
+
+            //File.WriteAllBytes("/Users/admin/Downloads/MyTex.png", tex.EncodeToPNG());
+        }
+
+        private byte[] GetByteArrayFromImage(Texture2D image)
+        {
+            byte[] bytesOfImage = image.EncodeToPNG();
+            return bytesOfImage;
+        }
+
+        private unsafe float[,,] GetImageAsFloats(IntPtr imageIntPtr, int width, int height)
+        {
+            byte* ptr = (byte*)imageIntPtr.ToPointer();
+            var input = new float[height, width, 1];
+
+            int offset = 0;
+            for (int i = 0; i < height; i++)
+            {
+                for (int j = 0; j < width; j++)
+                {
+                    input[i, j, 0] = (float)(ptr[offset + 2]);
+                    //float b = (float)ptr[offset + 0] / 255.0f;
+                    //float g = (float)ptr[offset + 1] / 255.0f;
+                    //float r = (float)ptr[offset + 2] / 255.0f;
+                    //float a = (float)ptr[offset + 3] / 255.0f;
+                    offset += 4;
+
+                    //UnityEngine.Color color = new UnityEngine.Color(r, g, b, a);
+                    //texture.SetPixel(j, height - i, color);
+                }
+            }
+
+            return input;
+        }
+    }
+}
---
+++ Packages/com.github.asus4.tflite/Runtime/InterpreterOptions.cs
@@ -0,0 +1,181 @@
+﻿/* Copyright 2018 The TensorFlow Authors. All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+
+using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+
+using TfLiteInterpreter = System.IntPtr;
+using TfLiteInterpreterOptions = System.IntPtr;
+using TfLiteTensor = System.IntPtr;
+using TfLiteDelegate = System.IntPtr;
+
+namespace TensorFlowLite
+{
+    public class InterpreterOptions : IDisposable
+    {
+        // void (*reporter)(void* user_data, const char* format, va_list args),
+        [UnmanagedFunctionPointer(CallingConvention.Cdecl, SetLastError = true)]
+        private delegate void ErrorReporterDelegate(IntPtr userData, string format, IntPtr argsPtrs);
+
+        internal TfLiteInterpreterOptions nativePtr;
+
+        private List<IGpuDelegate> delegates;
+
+        private int _threads;
+        public int threads
+        {
+            get => _threads;
+            set
+            {
+                _threads = value;
+                TfLiteInterpreterOptionsSetNumThreads(nativePtr, value);
+            }
+        }
+
+        private bool _useNNAPI;
+        public bool useNNAPI
+        {
+            get => _useNNAPI;
+            set
+            {
+                _useNNAPI = value;
+#if UNITY_ANDROID && !UNITY_EDITOR
+                InterpreterExtension.TfLiteInterpreterOptionsSetUseNNAPI(nativePtr, value);
+#endif // UNITY_ANDROID && !UNITY_EDITOR
+            }
+        }
+
+        public InterpreterOptions()
+        {
+            nativePtr = TfLiteInterpreterOptionsCreate();
+            delegates = new List<IGpuDelegate>();
+
+            TfLiteInterpreterOptionsSetErrorReporter(nativePtr, OnErrorReporter, IntPtr.Zero);
+        }
+
+        public void Dispose()
+        {
+            if (nativePtr != IntPtr.Zero)
+            {
+                TfLiteInterpreterOptionsDelete(nativePtr);
+            }
+            foreach (var gpuDelegate in delegates)
+            {
+                gpuDelegate.Dispose();
+            }
+            delegates.Clear();
+        }
+
+        public void AddGpuDelegate()
+        {
+            var gpuDelegate = CreateGpuDelegate();
+            if (gpuDelegate == null) return;
+            TfLiteInterpreterOptionsAddDelegate(nativePtr, gpuDelegate.Delegate);
+            delegates.Add(gpuDelegate);
+        }
+
+        [AOT.MonoPInvokeCallback(typeof(ErrorReporterDelegate))]
+        private static void OnErrorReporter(System.IntPtr userData, string format, IntPtr args)
+        {
+            // Marshalling va_list as args.
+            // refs:
+            // https://github.com/dotnet/runtime/issues/9316
+            // https://github.com/jeremyVignelles/va-list-interop-demo
+
+            string report;
+#if UNITY_EDITOR_WIN || UNITY_STANDALONE_WIN
+            // TODO: use vsprintf on windows
+            report = format;
+#else
+            int formatLength = printf(format, args);
+            IntPtr buffer = Marshal.AllocHGlobal(formatLength);
+            sprintf(buffer, format, args);
+            report = Marshal.PtrToStringAnsi(buffer);
+            Marshal.FreeHGlobal(buffer);
+#endif
+            UnityEngine.Debug.LogWarning($"Interperter Warning: {report}");
+        }
+
+#pragma warning disable CS0162 // Unreachable code detected 
+        private static IGpuDelegate CreateGpuDelegate()
+        {
+#if UNITY_ANDROID && !UNITY_EDITOR
+            return new GlDelegate();
+#elif UNITY_IOS || UNITY_EDITOR_OSX || UNITY_STANDALONE_OSX
+            return new MetalDelegate(new MetalDelegate.Options()
+            {
+                allowPrecisionLoss = true,
+                waitType = MetalDelegate.WaitType.Passive,
+                enableQuantization = false,
+            });
+#endif
+            UnityEngine.Debug.LogWarning("GPU Delegate is not supported on this platform");
+            return null;
+        }
+#pragma warning restore CS0162 // Unreachable code detected    
+
+
+        #region Externs
+
+#if UNITY_IOS && !UNITY_EDITOR
+        private const string TensorFlowLibrary = "__Internal";
+#else
+        private const string TensorFlowLibrary = "libtensorflowlite_c";
+#endif
+
+
+        [DllImport(TensorFlowLibrary)]
+        private static extern unsafe TfLiteInterpreterOptions TfLiteInterpreterOptionsCreate();
+
+        [DllImport(TensorFlowLibrary)]
+        private static extern unsafe void TfLiteInterpreterOptionsDelete(TfLiteInterpreterOptions options);
+
+        [DllImport(TensorFlowLibrary)]
+        private static extern unsafe void TfLiteInterpreterOptionsSetNumThreads(
+            TfLiteInterpreterOptions options,
+            int num_threads
+        );
+
+        [DllImport(TensorFlowLibrary)]
+        private static extern unsafe void TfLiteInterpreterOptionsAddDelegate(
+            TfLiteInterpreterOptions options,
+            TfLiteDelegate _delegate);
+
+        [DllImport(TensorFlowLibrary, CallingConvention = CallingConvention.Cdecl)]
+        private static extern void TfLiteInterpreterOptionsSetErrorReporter(
+            TfLiteInterpreterOptions options,
+            ErrorReporterDelegate errorReporter,
+            IntPtr user_data);
+
+
+#if !UNITY_EDITOR_WIN && !UNITY_STANDALONE_WIN
+        private const string LibCLibrary = "libc";
+
+        [DllImport(LibCLibrary, CharSet = CharSet.Ansi, CallingConvention = CallingConvention.Cdecl)]
+        extern static int printf(
+            [In][MarshalAs(UnmanagedType.LPStr)] string format,
+            IntPtr args);
+
+        [DllImport(LibCLibrary, CharSet = CharSet.Ansi, CallingConvention = CallingConvention.Cdecl)]
+        extern static int sprintf(
+            IntPtr buffer,
+            [In][MarshalAs(UnmanagedType.LPStr)] string format,
+            IntPtr args);
+#endif // !UNITY_EDITOR_WIN && !UNITY_STANDALONE_WIN
+
+        #endregion // Externs
+    }
+}
