119a4c3ecf6d56c97d5589cae7dcd3d5984f8bd7
Nikita Lukyantsev <>
2021-05-12 09:34:15 +0300

    Поменял логику формирования ссылки

--- Assets/Scripts/SettingsVPS.cs
+++ Assets/Scripts/SettingsVPS.cs
@@ -1,3 +1,4 @@
 ﻿using System.Collections;using System.Collections.Generic;using UnityEngine;namespace ARVRLab.VPSService{    public class SettingsVPS    {        // Server url        public string Url = "";        // Delay between sending        public float Timeout = 5;
 
-        public string defaultLocationId = "";        public SettingsVPS(string buildingName, string buildingGuid, ServerType serverType)        {            defaultLocationId = buildingGuid;            Url = URLController.CreateURL(buildingName, buildingGuid, serverType);        }        public SettingsVPS(string customUrl, string buildingGuid)        {            Url = customUrl;            defaultLocationId = buildingGuid;        }    }}
+        public string defaultLocationId = "";        public SettingsVPS(string buildingUrl, string buildingGuid)        {
+            Url = buildingUrl;            defaultLocationId = buildingGuid;        }    }}
